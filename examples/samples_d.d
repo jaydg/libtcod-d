c7fe1a135325a56472d55a40958b7013b0a54e10
Joachim de Groot <jdegroot@web.de>
2020-04-26 18:44:09 +0200

    More whitespace

--- examples/samples_d.d
+++ examples/samples_d.d
@@ -1348,209 +1348,209 @@ int effectNum = 0;
 float delay = 3.0f;
 
 void burn(SDL_Surface *screen, int samplex, int sampley, int samplew, int sampleh) {
-	int ridx=screen.format.Rshift/8;
-	int gidx=screen.format.Gshift/8;
-	int bidx=screen.format.Bshift/8;
-	int x,y;
-	for (x=samplex; x < samplex + samplew; x ++ ) {
-		Uint8 *p = cast(Uint8 *)screen.pixels + x * screen.format.BytesPerPixel + sampley * screen.pitch;
-		for (y=sampley; y < sampley + sampleh; y ++ ) {
-			int ir=0,ig=0,ib=0;
-			Uint8 *p2 = p + screen.format.BytesPerPixel; // get pixel at x+1,y
-			ir += p2[ridx];
-			ig += p2[gidx];
-			ib += p2[bidx];
-			p2 -= 2*screen.format.BytesPerPixel; // get pixel at x-1,y
-			ir += p2[ridx];
-			ig += p2[gidx];
-			ib += p2[bidx];
-			p2 += screen.format.BytesPerPixel+ screen.pitch; // get pixel at x,y+1
-			ir += p2[ridx];
-			ig += p2[gidx];
-			ib += p2[bidx];
-			p2 -= 2*screen.pitch; // get pixel at x,y-1
-			ir += p2[ridx];
-			ig += p2[gidx];
-			ib += p2[bidx];
-			ir/=4;
-			ig/=4;
-			ib/=4;
-			p[ridx]=cast(ubyte)ir;
-			p[gidx]=cast(ubyte)ig;
-			p[bidx]=cast(ubyte)ib;
-			p += screen.pitch;
-		}
-	}
+    int ridx=screen.format.Rshift/8;
+    int gidx=screen.format.Gshift/8;
+    int bidx=screen.format.Bshift/8;
+    int x,y;
+    for (x=samplex; x < samplex + samplew; x ++ ) {
+        Uint8 *p = cast(Uint8 *)screen.pixels + x * screen.format.BytesPerPixel + sampley * screen.pitch;
+        for (y=sampley; y < sampley + sampleh; y ++ ) {
+            int ir=0,ig=0,ib=0;
+            Uint8 *p2 = p + screen.format.BytesPerPixel; // get pixel at x+1,y
+            ir += p2[ridx];
+            ig += p2[gidx];
+            ib += p2[bidx];
+            p2 -= 2*screen.format.BytesPerPixel; // get pixel at x-1,y
+            ir += p2[ridx];
+            ig += p2[gidx];
+            ib += p2[bidx];
+            p2 += screen.format.BytesPerPixel+ screen.pitch; // get pixel at x,y+1
+            ir += p2[ridx];
+            ig += p2[gidx];
+            ib += p2[bidx];
+            p2 -= 2*screen.pitch; // get pixel at x,y-1
+            ir += p2[ridx];
+            ig += p2[gidx];
+            ib += p2[bidx];
+            ir/=4;
+            ig/=4;
+            ib/=4;
+            p[ridx]=cast(ubyte)ir;
+            p[gidx]=cast(ubyte)ig;
+            p[bidx]=cast(ubyte)ib;
+            p += screen.pitch;
+        }
+    }
 }
 
 void explode(SDL_Surface *screen, int samplex, int sampley, int samplew, int sampleh) {
-	int ridx=screen.format.Rshift/8;
-	int gidx=screen.format.Gshift/8;
-	int bidx=screen.format.Bshift/8;
-	int dist=cast(int)(10*(3.0f - delay));
-	int x,y;
-	for (x=samplex; x < samplex + samplew; x ++ ) {
-		Uint8 *p = cast(Uint8 *)screen.pixels + x * screen.format.BytesPerPixel + sampley * screen.pitch;
-		for (y=sampley; y < sampley + sampleh; y ++ ) {
-			int ir=0,ig=0,ib=0,i;
-			for (i=0; i < 3; i++) {
-				int dx = TCOD_random_get_int(null,-dist,dist);
-				int dy = TCOD_random_get_int(null,-dist,dist);
-				Uint8 *p2;
-				p2 = p + dx * screen.format.BytesPerPixel;
-				p2 += dy * screen.pitch;
-				ir += p2[ridx];
-				ig += p2[gidx];
-				ib += p2[bidx];
-			}
-			ir/=3;
-			ig/=3;
-			ib/=3;
-			p[ridx]=cast(ubyte)ir;
-			p[gidx]=cast(ubyte)ig;
-			p[bidx]=cast(ubyte)ib;
-			p += screen.pitch;
-		}
-	}
+    int ridx=screen.format.Rshift/8;
+    int gidx=screen.format.Gshift/8;
+    int bidx=screen.format.Bshift/8;
+    int dist=cast(int)(10*(3.0f - delay));
+    int x,y;
+    for (x=samplex; x < samplex + samplew; x ++ ) {
+        Uint8 *p = cast(Uint8 *)screen.pixels + x * screen.format.BytesPerPixel + sampley * screen.pitch;
+        for (y=sampley; y < sampley + sampleh; y ++ ) {
+            int ir=0,ig=0,ib=0,i;
+            for (i=0; i < 3; i++) {
+                int dx = TCOD_random_get_int(null,-dist,dist);
+                int dy = TCOD_random_get_int(null,-dist,dist);
+                Uint8 *p2;
+                p2 = p + dx * screen.format.BytesPerPixel;
+                p2 += dy * screen.pitch;
+                ir += p2[ridx];
+                ig += p2[gidx];
+                ib += p2[bidx];
+            }
+            ir/=3;
+            ig/=3;
+            ib/=3;
+            p[ridx]=cast(ubyte)ir;
+            p[gidx]=cast(ubyte)ig;
+            p[bidx]=cast(ubyte)ib;
+            p += screen.pitch;
+        }
+    }
 }
 
 void blur(SDL_Surface *screen, int samplex, int sampley, int samplew, int sampleh) {
-	// let's blur that sample console
-	float[3] f;
+    // let's blur that sample console
+    float[3] f;
         float n=0.0f;
-	int ridx=screen.format.Rshift/8;
-	int gidx=screen.format.Gshift/8;
-	int bidx=screen.format.Bshift/8;
-	int x,y;
-	f[2]=TCOD_sys_elapsed_seconds();
-	if ( noise == null ) noise=TCOD_noise_new(3, TCOD_NOISE_DEFAULT_HURST, TCOD_NOISE_DEFAULT_LACUNARITY, null);
-	for (x=samplex; x < samplex + samplew; x ++ ) {
-		Uint8 *p = cast(Uint8 *)screen.pixels + x * screen.format.BytesPerPixel + sampley * screen.pitch;
-		f[0]=cast(float)(x)/samplew;
-		for (y=sampley; y < sampley + sampleh; y ++ ) {
-			int ir=0,ig=0,ib=0,dec, count;
-			if ( (y-sampley)%8 == 0 ) {
-				f[1]=cast(float)(y)/sampleh;
-				n=TCOD_noise_get_fbm(noise,cast(float*)f,3.0f);
-			}
-			dec = cast(int)(3*(n+1.0f));
-			count=0;
-			switch(dec) {
-				case 4:
-					count += 4;
-					// get pixel at x,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p -= 2*screen.format.BytesPerPixel; // get pixel at x+2,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p -= 2*screen.pitch; // get pixel at x+2,y+2
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p+= 2*screen.format.BytesPerPixel; // get pixel at x,y+2
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p += 2*screen.pitch;
+    int ridx=screen.format.Rshift/8;
+    int gidx=screen.format.Gshift/8;
+    int bidx=screen.format.Bshift/8;
+    int x,y;
+    f[2]=TCOD_sys_elapsed_seconds();
+    if ( noise == null ) noise=TCOD_noise_new(3, TCOD_NOISE_DEFAULT_HURST, TCOD_NOISE_DEFAULT_LACUNARITY, null);
+    for (x=samplex; x < samplex + samplew; x ++ ) {
+        Uint8 *p = cast(Uint8 *)screen.pixels + x * screen.format.BytesPerPixel + sampley * screen.pitch;
+        f[0]=cast(float)(x)/samplew;
+        for (y=sampley; y < sampley + sampleh; y ++ ) {
+            int ir=0,ig=0,ib=0,dec, count;
+            if ( (y-sampley)%8 == 0 ) {
+                f[1]=cast(float)(y)/sampleh;
+                n=TCOD_noise_get_fbm(noise,cast(float*)f,3.0f);
+            }
+            dec = cast(int)(3*(n+1.0f));
+            count=0;
+            switch(dec) {
+                case 4:
+                    count += 4;
+                    // get pixel at x,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p -= 2*screen.format.BytesPerPixel; // get pixel at x+2,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p -= 2*screen.pitch; // get pixel at x+2,y+2
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p+= 2*screen.format.BytesPerPixel; // get pixel at x,y+2
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p += 2*screen.pitch;
                     goto case 3;
-				case 3:
-					count += 4;
-					// get pixel at x,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p += 2*screen.format.BytesPerPixel; // get pixel at x+2,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p += 2*screen.pitch; // get pixel at x+2,y+2
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p-= 2*screen.format.BytesPerPixel; // get pixel at x,y+2
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p -= 2*screen.pitch;
+                case 3:
+                    count += 4;
+                    // get pixel at x,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p += 2*screen.format.BytesPerPixel; // get pixel at x+2,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p += 2*screen.pitch; // get pixel at x+2,y+2
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p-= 2*screen.format.BytesPerPixel; // get pixel at x,y+2
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p -= 2*screen.pitch;
                     goto case 2;
-				case 2:
-					count += 4;
-					// get pixel at x,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p -= screen.format.BytesPerPixel; // get pixel at x-1,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p -= screen.pitch; // get pixel at x-1,y-1
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p+= screen.format.BytesPerPixel; // get pixel at x,y-1
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p += screen.pitch;
+                case 2:
+                    count += 4;
+                    // get pixel at x,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p -= screen.format.BytesPerPixel; // get pixel at x-1,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p -= screen.pitch; // get pixel at x-1,y-1
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p+= screen.format.BytesPerPixel; // get pixel at x,y-1
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p += screen.pitch;
                     goto case 1;
-				case 1:
-					count += 4;
-					// get pixel at x,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p += screen.format.BytesPerPixel; // get pixel at x+1,y
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p += screen.pitch; // get pixel at x+1,y+1
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p-= screen.format.BytesPerPixel; // get pixel at x,y+1
-					ir += p[ridx];
-					ig += p[gidx];
-					ib += p[bidx];
-					p -= screen.pitch;
-					ir/=count;
-					ig/=count;
-					ib/=count;
-					p[ridx]=cast(ubyte)ir;
-					p[gidx]=cast(ubyte)ig;
-					p[bidx]=cast(ubyte)ib;
-				break;
-				default:break;
-			}
-			p += screen.pitch;
-		}
-	}
+                case 1:
+                    count += 4;
+                    // get pixel at x,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p += screen.format.BytesPerPixel; // get pixel at x+1,y
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p += screen.pitch; // get pixel at x+1,y+1
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p-= screen.format.BytesPerPixel; // get pixel at x,y+1
+                    ir += p[ridx];
+                    ig += p[gidx];
+                    ib += p[bidx];
+                    p -= screen.pitch;
+                    ir/=count;
+                    ig/=count;
+                    ib/=count;
+                    p[ridx]=cast(ubyte)ir;
+                    p[gidx]=cast(ubyte)ig;
+                    p[bidx]=cast(ubyte)ib;
+                break;
+                default:break;
+            }
+            p += screen.pitch;
+        }
+    }
 }
 
 extern(C) void SDL_render(void *sdlSurface) {
-	SDL_Surface *screen = cast(SDL_Surface *)sdlSurface;
-	// now we have almighty access to the screen's precious pixels !!
-	// get the font character size
-	int charw,charh,samplex,sampley;
-	TCOD_sys_get_char_size(&charw,&charh);
-	// compute the sample console position in pixels
-	samplex = SAMPLE_SCREEN_X * charw;
-	sampley = SAMPLE_SCREEN_Y * charh;
-	delay -= TCOD_sys_get_last_frame_length();
-	if ( delay < 0.0f ) {
-		delay = 3.0f;
-		effectNum = (effectNum + 1) % 3;
-		if ( effectNum == 2 ) sdl_callback_enabled=false; // no forced redraw for burn effect
-		else sdl_callback_enabled=true;
-	}
-	switch(effectNum) {
-		case 0 : blur(screen,samplex,sampley,SAMPLE_SCREEN_WIDTH * charw,SAMPLE_SCREEN_HEIGHT * charh); break;
-		case 1 : explode(screen,samplex,sampley,SAMPLE_SCREEN_WIDTH * charw,SAMPLE_SCREEN_HEIGHT * charh); break;
-		case 2 : burn(screen,samplex,sampley,SAMPLE_SCREEN_WIDTH * charw,SAMPLE_SCREEN_HEIGHT * charh); break;
+    SDL_Surface *screen = cast(SDL_Surface *)sdlSurface;
+    // now we have almighty access to the screen's precious pixels !!
+    // get the font character size
+    int charw,charh,samplex,sampley;
+    TCOD_sys_get_char_size(&charw,&charh);
+    // compute the sample console position in pixels
+    samplex = SAMPLE_SCREEN_X * charw;
+    sampley = SAMPLE_SCREEN_Y * charh;
+    delay -= TCOD_sys_get_last_frame_length();
+    if ( delay < 0.0f ) {
+        delay = 3.0f;
+        effectNum = (effectNum + 1) % 3;
+        if ( effectNum == 2 ) sdl_callback_enabled=false; // no forced redraw for burn effect
+        else sdl_callback_enabled=true;
+    }
+    switch(effectNum) {
+        case 0 : blur(screen,samplex,sampley,SAMPLE_SCREEN_WIDTH * charw,SAMPLE_SCREEN_HEIGHT * charh); break;
+        case 1 : explode(screen,samplex,sampley,SAMPLE_SCREEN_WIDTH * charw,SAMPLE_SCREEN_HEIGHT * charh); break;
+        case 2 : burn(screen,samplex,sampley,SAMPLE_SCREEN_WIDTH * charw,SAMPLE_SCREEN_HEIGHT * charh); break;
                 default: assert(false);
-	}
+    }
 }
 
 
